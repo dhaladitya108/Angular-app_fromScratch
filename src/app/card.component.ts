71ee7b4868480411149e13bfc59af92fad0d05e8
Aditya Prasad Dhal <dhaladitya108@gmail.com>
2020-06-16 22:19:59 +0530

    ☸️

---
+++ src/app/card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'card-app',
+  templateUrl: 'card.component.html',
+  styleUrls: ['card.component.css'],
+})
+export class CardComponent {}
